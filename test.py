f0bb48ce1b0100d17fba2bf40d1e6150240d271d
Charlotte Natasha <natashaonyango97@gmail.com>
2022-04-25 00:19:59 +0200

    added credential initializer

--- test.py
+++ test.py
@@ -9,3 +9,19 @@ class UserTest(unittest.TestCase):
 
     unittest.testcase: helps in creating test cases
     """
+
+    def setUp(self):
+        """
+        Runs before each test case
+        :return:
+        """
+        self.new_user = Credentials("Instagram", "Charlotte", "#123")
+
+    def test_init(self):
+        """
+        Checks if the function is initialized properly
+        :return:
+        """
+        self.assertEqual(self.new_user.account, "Instagram")
+        self.assertEqual(self.new_user.username, "Charlotte")
+        self.assertEqual(self.new_user.password, "#123")
